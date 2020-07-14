4e28b511b01b1a98912d9d34468a9a0d68977ca5
Khenus <khenus.tan@gmail.com>
2020-07-14 23:49:35 +0800

    V1.16 Full commit with node_modules

---
+++ FinCal/node_modules/react-native-tabs-section-list/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SectionList_1 = require("./SectionList");
+exports.default = SectionList_1.default;
+//# sourceMappingURL=index.js.map
